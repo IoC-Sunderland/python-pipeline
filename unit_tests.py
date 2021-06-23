b00b10ea05c9af70904f4a09b3883f751e22c9e4
Institute of Coding (Sunderland) <gavin.mcclary@sunderland.ac.uk>
2021-06-23 15:49:41 +0100

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -3,9 +3,8 @@ from lambda_function import my_func
 
 class TestLambdaFunction(unittest.TestCase):
 
-    def test_my_func_return_type(self):
+    def test_my_func_returns_uppercase_string(self):
         self.assertEqual(my_func('foo'), 'FOO')
     
-
 if __name__ == '__main__':
     unittest.main()
